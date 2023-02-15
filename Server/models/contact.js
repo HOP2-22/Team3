301453efb0a974a6fccc96b3026720d8a6be2e50
dulguuntengis <dulguuntengis@Bataas-iMac.local>
2023-02-15 18:31:31 +0800

    contact

--- Server/models/contact.js
+++ Server/models/contact.js
@@ -1,6 +1,18 @@
 const mongoose = require("mongoose");
 
-const contactSchema = new mongoose.Schema({});
+const contactSchema = new mongoose.Schema({
+    email : {
+        type : "string",
+        required : true
+    },
+    text : {
+        type : "string",
+        required : true
+    },
+    name : {
+        type : "string",
+    }
+});
 
 const Contact = mongoose.model("contacts", contactSchema);
 
