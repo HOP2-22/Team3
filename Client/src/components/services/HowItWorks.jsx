9da49a265a306b2eaa7428efc681df46dcd63731
Itgel69 <itgel007@gmail.com>
2023-02-15 18:48:25 +0800

    ok

--- Client/src/components/services/HowItWorks.jsx
+++ Client/src/components/services/HowItWorks.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import "../../css/HowItWorks.css";
 const HowItWorks = () => {
   return (
-    <div>
-      <h1>How It works</h1>
-      <div>
-        You can make a post about your thoughts. People will agree or disagree
-        about your thoughts in the comment section. This is also your hub for
-        teamwork. Give everyone you work with—inside and outside your company—a
-        more productive way to stay in sync.Respond faster with emoji, keep conversations focused in channels, and simplify all your communication into one place.
+    <div style={{ marginTop: "69px" }}>
+      <div className="HowItWorks">
+        <h1>How It works</h1>
+        <div>
+          You can make a post about your thoughts. People will agree or disagree
+          about your thoughts in the comment section. This is also your hub for
+          teamwork. Give everyone you work with—inside and outside your
+          company—a more productive way to stay in sync.Respond faster with
+          emoji, keep conversations focused in channels, and simplify all your
+          communication into one place.
+        </div>
       </div>
     </div>
   );
