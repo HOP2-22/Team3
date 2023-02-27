e38a0042b046f0ecf45de817295d3d5123c82592
dulguuntengis <dulguuntengis@Bataas-iMac.local>
2023-02-27 19:25:19 +0800

    new

--- Client/src/pages/auth/Signup.jsx
+++ Client/src/pages/auth/Signup.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Signup = () => {
-  return <div className="">Signup</div>;
+  return (
+    <div style={{ marginTop: "100px" }}>
+      <div>
+        <div>
+          <div>Welcome</div>
+          <div>
+            <div>Already have a account</div>
+            <div>Sign in</div>
+          </div>
+        </div>
+        <input placeholder="email" />
+      </div>
+      <img src="nature.png" />
+    </div>
+  );
 };
 
 export default Signup;
